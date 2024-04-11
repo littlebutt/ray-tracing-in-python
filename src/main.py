5dbae338473fcab684bc5975a7bce045cf1a9f45
littlebutt <luogan199686@gmail.com>
2024-04-12 07:51:44 +0800

    feat: support dielectric material

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from camera import Camera
-from material import Lambertian, Metal
+from material import Dielectric, Lambertian, Metal
 from sphere import Sphere
 from vec import Color, Point3
 from world import World
@@ -14,13 +14,13 @@ if __name__ == '__main__':
 
     material_ground = Lambertian(Color(0.8, 0.8, 0.0))
     material_center = Lambertian(Color(0.1, 0.2, 0.5))
-    material_left = Metal(Color(0.8, 0.8, 0.8), 0.3)
+    material_left = Dielectric(1.5)
     material_right = Metal(Color(0.8, 0.6, 0.2))
     
     world = World()
     world.add(Sphere(Point3(0.0, -100.5, -1.0), 100.0, material_ground))
     world.add(Sphere(Point3(0.0, 0.0, -1.2), 0.5, material_center))
-    world.add(Sphere(Point3(-1.0, 0.0, -1.0), 0.5, material_left))
+    world.add(Sphere(Point3(-1.0, 0.0, -1.2), 0.5, material_left))
     world.add(Sphere(Point3(1.0, 0.0, -1.2), 0.5, material_right))
 
     cam = Camera(aspect_ratio, image_width, samples_per_pixel, 50)
--- src/material.py
+++ src/material.py
@@ -1,17 +1,21 @@
+from math import sqrt
 from typing import Tuple
 from hittable import HitRecord
 from ray import Ray
-from utils import near_zero, random_unit_vector
+from utils import near_zero, random_float, random_unit_vector
 from vec import Color, Vector3
 
 
-__all__ = ['Material', 'Lambertian', 'Metal']
+__all__ = ['Material', 'Lambertian', 'Metal', 'Dielectric']
 
 
 class Material:
 
     def scatter(self, r_in: "Ray", rec: "HitRecord") -> Tuple[bool, "Color", "Ray"]:
         return False
+    
+    def _reflect(self, v: "Vector3", n: "Vector3") -> "Vector3":
+        return v - 2 * v.dot(n) * n
 
 
 class Lambertian(Material):
@@ -37,7 +41,32 @@ class Metal(Material):
         reflected = reflected.unit_vector() + self.fuzz * random_unit_vector()
         scattered = Ray(rec.p, reflected)
         return scattered.direction().dot(rec.normal) > 0, self.albedo, scattered
+
+
+class Dielectric(Material):
+
+    def __init__(self, refraction_index: float) -> None:
+        self.refraction_index = refraction_index
+
+    def scatter(self, r_in: Ray, rec: HitRecord) -> Tuple[bool | Color | Ray]:
+        ri = 1.0 / self.refraction_index if rec.front_face else self.refraction_index
+        unit_direction = r_in.direction().unit_vector()
+        cos_theta = min(rec.normal.dot(-unit_direction), 1.0)
+        sin_theta = sqrt(1.0 - cos_theta * cos_theta)
+        if ri * sin_theta > 1.0 or self._reflectance(cos_theta, ri) > random_float():
+            direction = self._reflect(unit_direction, rec.normal)
+        else:
+            direction = self._refract(unit_direction, rec.normal, ri)
+        return True, Color(1.0, 1.0, 1.0), Ray(rec.p, direction)
     
-    def _reflect(self, v: "Vector3", n: "Vector3") -> "Vector3":
-        return v - 2 * v.dot(n) * n
+    def _refract(self, uv: "Vector3", n: "Vector3", etai_over_eta: float) -> "Vector3":
+        cos_theta = min(n.dot(-uv), 1.0)
+        r_out_prep = etai_over_eta * (uv + cos_theta * n)
+        r_out_parallel = -sqrt(abs(1.0 - r_out_prep.length_squared())) * n
+        return r_out_prep + r_out_parallel
+    
+    def _reflectance(self, cosine: float, refraction_index: float) -> float:
+        r0 = (1 - refraction_index) / (1 + refraction_index)
+        r0 = r0 * r0
+        return r0 + (1 - r0) * pow((1 - cosine), 5)
     
