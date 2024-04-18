db5edd2992df2f866747d4718ec4934140f88397
littlebutt <luogan199686@gmail.com>
2024-04-19 07:29:29 +0800

    doc: comment for `hittable.py`

--- src/hittable.py
+++ src/hittable.py
@@ -8,6 +8,17 @@ __all__ = ['HitRecord', 'Hittable']
 
 
 class HitRecord:
+    '''
+    Record the information when the :class:`Ray` hit a :class:`Hittable`s.
+
+    Attributes:
+        p: The hit point in :class:`Point3` format.
+        normal: The normal vector of the hit point in :class:`Point3` format.
+        t: The parameter ``t`` in the ray equation.
+        front_face: If the :args:`normal` points outward of the surface.
+        mat: The :class:`Material` of the surface.
+
+    '''
 
     # XXX: The type of :args:`mat` is :class:`Material`. We use :type:`Any` here
     # is avoiding circular import.
@@ -19,6 +30,7 @@ class HitRecord:
         self.mat = mat
     
     def set_face_normal(self, ray: "Ray", outward_normal: "Vector3") -> None:
+        '''Set the front face of the hit point.'''
         front_face = ray.direction().dot(outward_normal)
         self.normal = outward_normal if front_face < 0 else -outward_normal
 
@@ -27,4 +39,17 @@ class HitRecord:
 class Hittable:
 
     def hit(self, ray: "Ray", interval: "Interval") -> Tuple[bool, Optional["HitRecord"]]:
+        '''
+        Detect if the :class:`Ray` can hit the target within the interval. If hittable, it will 
+        return its :class:`HitRecord`.
+
+        Args:
+            ray: The ray equation for detecting.
+            interval: The given :class:`Interval`.
+        
+        Returns:
+            bool: If the ray can hit it.
+            HitRecord: The :class:`HitRecord` of the hit point if hittable.
+
+        '''
         pass
